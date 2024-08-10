f2936119476009cc9f27c62352b88015cecce891
AdishtJaglan <adisht7524@gmail.com>
2024-08-10 22:18:04 +0530

    create contact me page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import {
   Navigate,
 } from "react-router-dom";
 import Home from "./pages/Home";
+import ContactMe from "./pages/ContactMe";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/home" element={<Home />}></Route>
+        <Route path="/contact-me" element={<ContactMe />}></Route>
         <Route path="*" element={<Navigate to="/home" />}></Route>
       </Routes>
     </Router>
---
+++ client/src/pages/ContactMe.jsx
@@ -0,0 +1,9 @@
+import ContactForm from "../components/ContactForm";
+
+export default function ContactMe() {
+  return (
+    <div className="min-h-screen bg-light dark:bg-dark">
+      <ContactForm />
+    </div>
+  );
+}
